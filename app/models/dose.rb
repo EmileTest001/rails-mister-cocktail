d430a536b17cad0624e0b5d0f74547db3728b1f3
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-12 13:54:34 +0100

    V1 MVC Coctails, missing : (dose, ingredient) per cocktail

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  # validates :cocktail_id, :ingredient_id
-  # validates_uniqueness_of :cocktail_id, :ingredient_id
   validates :ingredient_id, uniqueness: { scope: :cocktail_id }
 end
