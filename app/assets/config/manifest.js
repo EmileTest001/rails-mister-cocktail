964dc7bccc3f2a081690f393d5b1194b50a22a23
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-14 02:23:29 +0100

    upload image locally OK

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+// 14.11.2020 Cloudinary : for direct uploading
+//= require activestorage
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,8 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
+  # 13.11.2020 Rails Day 5 Cloudinary configuration
+  has_one_attached :photo
+
   validates :name, uniqueness: true, presence: true
 end
